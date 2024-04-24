75b0b1ab32e0e23d46bc3a3fede70fa662b3cb16
Noel Egberto Meléndez Sequeira <31997312+masmian@users.noreply.github.com>
2024-04-24 13:09:48 -0600

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -8,7 +8,8 @@ load_dotenv()
 TURSO_DATABASE_URL = "libsql://fastapi-database-masmian.turso.io"
 TURSO_AUTH_TOKEN = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTM0ODAxNDAsImlkIjoiNWUzMTJhYjEtZjA5MS00MDZjLWE5YjgtOWEwOWNmZmRlZTU5In0.dwZXm16rwJtImyn0Qfjoo3cZJD6p5KCpWdEWWUzTJD_i9rPCsuAaaRyU2XaT882VaAkq_kh85MLgCMfZNooaAQ"
 
-dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+dbUrl = f"sqlite:///database.db"
+#dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
 engine = create_engine(dbUrl)
 SesionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
